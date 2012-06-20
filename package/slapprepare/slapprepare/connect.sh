c2e470092c395ed443cb6774ca6292fe82384b7d
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2012-06-20 20:22:31 -0300

    Corrected syntax of questions for new computer to be more readable

--- package/slapprepare/slapprepare/connect.sh
+++ package/slapprepare/slapprepare/connect.sh
@@ -5,8 +5,8 @@ TEMP_DIR="/tmp/slaptemp/"
 BASEURL="https://www.vifib.net"
 
 while :; do
-    read -p "Enter your user name:" USERNAME
-    read -s -p "Enter your password:" PASSWORD
+    read -p "Enter your user name: " USERNAME
+    read -s -p "Enter your password: " PASSWORD
     echo ""
     curl -k --location --cookie cookie.txt  --user $USERNAME:$PASSWORD "$BASEURL" | grep logout > /dev/null
     if [[ $? != 0 ]]; then
@@ -17,7 +17,7 @@ while :; do
     fi
 done
 
-echo "Enter Computer name"
+echo "Enter Computer name: "
 read COMPUTERNAME
     
 curl -k -o "$TEMP_DIR/certificate_key.html" --location --cookie cookie.txt --user $USERNAME:$PASSWORD "https://www.vifib.net/add-a-server/WebSection_registerNewComputer?dialog_id=WebSection_viewServerInformationDialog&dialog_method=WebSection_registerNewComputer&title=$COMPUTERNAME&object_path=/erp5/web_site_module/hosting/add-a-server&update_method=&cancel_url=https%3A//www.vifib.net/add-a-server/WebSection_viewServerInformationDialog&Base_callDialogMethod=&field_your_title=Essai1&dialog_category=None&form_id=view"
